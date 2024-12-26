c61efa53a316354c35959dbeb4f5d222aa62d9f3
damhs <ihansaem1@kaist.ac.kr>
2024-12-26 21:00:17 +0900

    FEAT: gallery tab image

--- lib/gallery_tab.dart
+++ lib/gallery_tab.dart
@@ -5,12 +5,44 @@ class GalleryTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      // 실제 검색 UI를 만들기 전, 간단한 텍스트 표시
-      child: Text(
-        '갤러리 탭',
-        style: TextStyle(fontSize: 22),
+    final sizeX = MediaQuery.of(context).size.width;
+    final sizeY = MediaQuery.of(context).size.height;
+    return Container(
+      width: sizeX,
+      height: sizeY,
+      child: GridView.count(
+        scrollDirection: Axis.vertical,
+        crossAxisCount: 2,
+        children: createGallery(6),
+        mainAxisSpacing: 5.0,
+        crossAxisSpacing: 5.0,
+        padding: EdgeInsets.all(5.0),
       ),
     );
+  }
+
+  List<Widget> createGallery(int num) {
+    List<Widget> images = [];
+    List<String> urls = [];
+    urls.add('img/img1.jpeg');
+    urls.add('img/img2.jpeg');
+    urls.add('img/img3.jpeg');
+    urls.add('img/img4.jpeg');
+    urls.add('img/img5.jpeg');
+    urls.add('img/img6.jpeg');
+
+    Widget image;
+    for (int i = 0; i < num; i++) {
+      image = Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(urls[i]),
+            fit: BoxFit.cover,
+          ),
+        ),
+      );
+      images.add(image);
+    }
+    return images;
   }
 }
