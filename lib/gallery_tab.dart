9bbf11db3a17dd3d75169154a04e43e35f8da9f7
damhs <ihansaem1@kaist.ac.kr>
2024-12-28 13:41:37 +0900

    FEAT: detail page in gallery tab

--- lib/gallery_tab.dart
+++ lib/gallery_tab.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 
 class GalleryTab extends StatefulWidget {
   GalleryTab({Key? key}) : super(key: key);
@@ -14,22 +15,58 @@ class GalleryTab extends StatefulWidget {
 class _GalleryTabState extends State<GalleryTab> {
   // 초기 이미지와 설명 리스트
   final List<Map<String, String>> items = [
-    {'image': 'img/img1.jpeg', 'description': '자유로부터의 도피'},
-    {'image': 'img/img2.jpeg', 'description': '모비딕'},
-    {'image': 'img/img3.jpeg', 'description': '채식주의자'},
-    {'image': 'img/img4.jpeg', 'description': '철학'},
-    {'image': 'img/img5.jpeg', 'description': '채식주의자'},
-    {'image': 'img/img6.jpeg', 'description': '철학'},
+    {
+      'image': 'img/img1.jpeg',
+      'description': '자유로부터의 도피',
+      'timestamp': '2024-12-28 12:00:00'
+    },
+    {
+      'image': 'img/img2.jpeg',
+      'description': '모비딕',
+      'timestamp': '2024-12-28 12:00:00'
+    },
+    {
+      'image': 'img/img3.jpeg',
+      'description': '채식주의자',
+      'timestamp': '2024-12-28 12:00:00'
+    },
+    {
+      'image': 'img/img4.jpeg',
+      'description': '철학',
+      'timestamp': '2024-12-28 12:00:00'
+    },
+    {
+      'image': 'img/img5.jpeg',
+      'description': '채식주의자',
+      'timestamp': '2024-12-28 12:00:00'
+    },
+    {
+      'image': 'img/img6.jpeg',
+      'description': '철학',
+      'timestamp': '2024-12-28 12:00:00'
+    },
   ];
 
   final ImagePicker _picker = ImagePicker();
 
   // 갤러리에서 이미지를 선택
   Future<void> _pickImage() async {
+    final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    final AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+    final int androidVersion = int.parse(androidInfo.version.release);
+    bool havePermission = false;
+
+    if (androidVersion >= 13) {
+      final request = await Permission.photos.request();
+      havePermission = request.isGranted;
+    } else {
+      final status = await Permission.storage.request();
+      havePermission = status.isGranted;
+    }
+
     // 권한 요청
-    final status = await Permission.photos.request();
     print("권한 요청");
-    if (status.isGranted) {
+    if (havePermission) {
       final XFile? pickedFile =
           await _picker.pickImage(source: ImageSource.gallery);
       if (pickedFile != null) {
@@ -41,6 +78,7 @@ class _GalleryTabState extends State<GalleryTab> {
           items.add({
             'image': pickedFile.path,
             'description': description,
+            'timestamp': DateTime.now().toString()
           });
         });
       }
@@ -92,47 +130,114 @@ class _GalleryTabState extends State<GalleryTab> {
         backgroundColor: Colors.white,
       ),
       body: Container(
-          width: sizeX,
-          height: sizeY,
-          child: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 5.0,
-              mainAxisSpacing: 5.0,
-              childAspectRatio: 0.75,
-            ),
-            itemCount: items.length,
-            padding: const EdgeInsets.all(5.0),
-            itemBuilder: (context, index) {
-              return Column(
+        width: sizeX,
+        height: sizeY,
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 5.0,
+            mainAxisSpacing: 5.0,
+            childAspectRatio: 1.0,
+          ),
+          itemCount: items.length,
+          padding: const EdgeInsets.all(5.0),
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetailPage(
+                      image: items[index]['image']!,
+                      description: items[index]['description']!,
+                    ),
+                  ),
+                );
+              },
+              child: Stack(
                 children: [
-                  Expanded(
+                  Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: index < 6
+                            ? AssetImage(items[index]['image']!)
+                                as ImageProvider
+                            : FileImage(File(items[index]['image']!)),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.white,
+                        width: 1.5,
+                      ),
+                    ),
+                    margin: const EdgeInsets.all(5.0),
+                  ),
+                  Positioned(
+                    bottom: 5,
+                    left: 5,
                     child: Container(
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: index < 6
-                              ? AssetImage(items[index]['image']!)
-                                  as ImageProvider
-                              : FileImage(File(items[index]['image']!)),
-                          fit: BoxFit.cover,
+                      padding: const EdgeInsets.all(5.0),
+                      child: Text(
+                        items[index]['timestamp']!,
+                        style: const TextStyle(
+                          fontSize: 12.0,
+                          color: Colors.white,
                         ),
                       ),
                     ),
                   ),
-                  const SizedBox(height: 8),
-                  Text(
-                    items[index]['description']!,
-                    style: const TextStyle(fontSize: 16),
-                  ),
-                  const SizedBox(height: 8),
                 ],
-              );
-            },
-          )),
+              ),
+            );
+          },
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: _pickImage,
         backgroundColor: Color(0xFF33CCCC),
         child: const Icon(Icons.add, color: Colors.white),
+      ),
+    );
+  }
+}
+
+class DetailPage extends StatelessWidget {
+  final String image;
+  final String description;
+
+  const DetailPage({Key? key, required this.image, required this.description})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('상세보기'),
+        backgroundColor: Colors.white,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(image!) as ImageProvider,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              description,
+              style: const TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
       ),
     );
   }
